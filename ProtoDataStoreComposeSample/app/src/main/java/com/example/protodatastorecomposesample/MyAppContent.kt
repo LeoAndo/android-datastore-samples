13628fd633a2bb6832cd24532c61d468eb4613ec
LeoAndo <leo.777.info@gmail.com>
2022-02-19 00:50:14 +0900

    create Home Screen

--- ProtoDataStoreComposeSample/app/src/main/java/com/example/protodatastorecomposesample/MyAppContent.kt
+++ ProtoDataStoreComposeSample/app/src/main/java/com/example/protodatastorecomposesample/MyAppContent.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.protodatastorecomposesample.ui.home.HomeScreen
 import com.example.protodatastorecomposesample.ui.theme.ProtoDataStoreComposeSampleTheme
 
 @Composable
@@ -64,7 +65,7 @@ private fun MyAppNavigationGraph(
     ) {
 
         composable(route = TopDestinations.HomeRoute.routeName, content = {
-            Text(text = "home")
+            HomeScreen()
         })
 
         composable(route = TopDestinations.AccountRoute.routeName, content = {
---
+++ ProtoDataStoreComposeSample/app/src/main/java/com/example/protodatastorecomposesample/ui/home/HomeViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.protodatastorecomposesample.ui.home
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+internal class HomeViewModel @Inject constructor() : ViewModel() {
+    var uiState by mutableStateOf<HomeUiState>(HomeUiState.Initial)
+        private set
+
+    init {
+        // TODO: data storeの最新の情報を取得する(Flow)
+    }
+
+    fun savePersonData(
+        id: String,
+        name: String,
+        height: String,
+        student: Boolean,
+        phoneNumber: String,
+        phoneTypeOrdinal: Int
+    ) {
+        uiState = HomeUiState.Loading
+        viewModelScope.launch {
+
+            // uiState = HomeUiState.Error(it.localizedMessage ?: "error.")
+        }
+    }
+}
