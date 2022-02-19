e4d4ad69c9a3d50aab979515928fc66711eee555
LeoAndo <leo.777.info@gmail.com>
2022-02-19 23:38:16 +0900

    replace Scaffold

--- ProtoDataStoreComposeSample/app/src/main/java/com/example/protodatastorecomposesample/MyAppContent.kt
+++ ProtoDataStoreComposeSample/app/src/main/java/com/example/protodatastorecomposesample/MyAppContent.kt
@@ -1,55 +1,47 @@
 package com.example.protodatastorecomposesample
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.protodatastorecomposesample.ui.account.AccountScreen
+import com.example.protodatastorecomposesample.ui.extentions.mainContentPadding
 import com.example.protodatastorecomposesample.ui.home.HomeScreen
 import com.example.protodatastorecomposesample.ui.theme.ProtoDataStoreComposeSampleTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun MyAppContent() {
     ProtoDataStoreComposeSampleTheme {
         var selectedItem by remember { mutableStateOf(0) }
         val items = listOf(TopDestinations.HomeRoute, TopDestinations.AccountRoute)
 
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background,
-            content = {
-                Column(modifier = Modifier.fillMaxSize()) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
-                    ) {
-                        MyAppNavigationGraph(
-                            modifier = Modifier.padding(PaddingValues(12.dp)),
-                            startDestination = items[selectedItem].routeName
+        Scaffold(
+            bottomBar = {
+                NavigationBar {
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            icon = { Icon(item.icon, contentDescription = item.label) },
+                            label = { Text(item.label) },
+                            selected = selectedItem == index,
+                            onClick = { selectedItem = index }
                         )
                     }
-
-                    Column {
-                        NavigationBar {
-                            items.forEachIndexed { index, item ->
-                                NavigationBarItem(
-                                    icon = { Icon(item.icon, contentDescription = item.label) },
-                                    label = { Text(item.label) },
-                                    selected = selectedItem == index,
-                                    onClick = { selectedItem = index }
-                                )
-                            }
-                        }
-                    }
                 }
-            }
-        )
+            },
+            content = {
+                Box(
+                    modifier = Modifier.mainContentPadding(it)
+                ) {
+                    MyAppNavigationGraph(startDestination = items[selectedItem].routeName)
+                }
+            })
     }
 }
 
